ae682fedc1f41e6363940791cd729ac032df91d8
Admin <admin@Admins-iMac.local>
2023-02-09 09:28:00 +0000

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>
+        Hello World {{ name }}
+    </h1>
+</template>
+
+<script lang="ts" setup>
+import { PropType } from 'vue';
+
+defineProps({
+    name: String
+})
+</script>
+
+<style>
+
+</style>
